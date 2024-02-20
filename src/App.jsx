8c63d7110d44a397ada88fa94f0b2e224cf3cdc3
sno0wi <sno0wi20@gmail.com>
2024-02-20 17:52:25 +0200

    task1(styles need to be finalized)

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
 import './App.css';
+import Profile from './components/Profile';
+import userData from './userData.json';
 
 function App() {
-  return <div></div>;
+  return (
+    <>
+      <Profile
+        avatar={userData.avatar}
+        username={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        followers={userData.stats.followers}
+        views={userData.stats.views}
+        likes={userData.stats.likes}
+      />
+    </>
+  );
 }
 
 export default App;
