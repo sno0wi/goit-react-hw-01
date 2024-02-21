8d1bb0beb66eb5b8cdc0c4f097b5734e00ab4a4a
sno0wi <sno0wi20@gmail.com>
2024-02-21 21:13:29 +0200

    add clsx to task 2

--- src/components/FriendList/FriendListItem.jsx
+++ src/components/FriendList/FriendListItem.jsx
@@ -1,18 +1,17 @@
 import styles from './FriendList.module.css';
+import clsx from 'clsx';
+
 const FriendListItem = ({ avatar, name, isOnline }) => {
+  const statusClassName = clsx(styles.status, {
+    [styles.online]: isOnline,
+    [styles.offline]: !isOnline,
+  });
+  const statusText = isOnline ? 'Online' : 'Offline';
   return (
     <li className={styles.item}>
       <img className="img" src={avatar} alt="Avatar" width="48" />
       <p className="name">{name}</p>
-      {isOnline ? (
-        <p style={{ color: 'green' }} className="status">
-          Online
-        </p>
-      ) : (
-        <p style={{ color: 'red' }} className="status">
-          Offline
-        </p>
-      )}
+      <p className={statusClassName}>{statusText}</p>
     </li>
   );
 };
