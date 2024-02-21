c5ee0cd5605cd44df3389746cc82d04fdca38e92
sno0wi <sno0wi20@gmail.com>
2024-02-21 20:02:01 +0200

    fixed FriendList

--- src/components/React-FriendList/FriendList.jsx
+++ src/components/React-FriendList/FriendList.jsx
@@ -4,14 +4,16 @@ import styles from './FriendList.module.css';
 const FriendList = ({ friends }) => {
   return (
     <ul className={styles.list}>
-      {friends.map(friend => (
-        <FriendListItem
-          key={friend.id}
-          avatar={friend.avatar}
-          name={friend.name}
-          isOnline={friend.isOnline}
-        />
-      ))}
+      {friends.map(friend => {
+        return (
+          <FriendListItem
+            key={friend.id}
+            avatar={friend.avatar}
+            name={friend.name}
+            isOnline={friend.isOnline}
+          />
+        );
+      })}
     </ul>
   );
 };
